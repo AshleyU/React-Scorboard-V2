b4965e19a01dd0d0b54726c22766c1a61fb53322
Ashley U <ashley.umetsu@gmail.com>
2021-07-16 15:38:26 -0500

    Adding a form to AddPlayerForm

--- scoreboard/src/components/AddPlayerForm.js
+++ scoreboard/src/components/AddPlayerForm.js
@@ -4,7 +4,17 @@ class AddPlayerForm extends Component {
 
     render() {
         return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Enter a player's name"
+                />
 
+                <input
+                    type="submit"
+                    value="Add Player"
+                />
+            </form>
         );
     }
 
